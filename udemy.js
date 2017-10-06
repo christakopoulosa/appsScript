e06ecb196b1334ee563bc6c943fc7a910bd528c4
christakopoulosa <argyrios.christakopoulos@gmail.com>
2017-10-06 16:11:21 +0100

    created .js file and linked it to udemy.html

---
+++ udemy.js
@@ -0,0 +1,4 @@
+$.getJSON("https://spreadsheets.google.com/feeds/list/1LV-tovyMbh8evUhlceF33h6UWypIJKUaOQpOQYc75ac/od6/public/values?alt=json",
+    function (data) {
+        console.log(data);
+    });
