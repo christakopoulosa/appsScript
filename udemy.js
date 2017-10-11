22380ec8df6948138bfe8eea1f4509d8f6eafe15
christakopoulosa <argyrios.christakopoulos@gmail.com>
2017-10-11 13:00:45 +0100

    deleted .js page and added code to .html page

--- udemy.js
+++
@@ -1,4 +0,0 @@
-$.getJSON("https://spreadsheets.google.com/feeds/list/1LV-tovyMbh8evUhlceF33h6UWypIJKUaOQpOQYc75ac/od6/public/values?alt=json",
-    function (data) {
-        console.log(data);
-    });
